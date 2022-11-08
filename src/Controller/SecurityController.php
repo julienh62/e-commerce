c7ea4085440dd6f4e902b66ebbd22df0bec16641
LAPTOP-C6IOEE1R <jhennebo@gmail.com>
2022-11-08 17:14:49 +0100

    suite-mot-passe-oublie

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\ResetPasswordRequestFormType;
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -33,4 +36,36 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/oubli-pass', name: 'forgotten_password')]
+    public function forgottenPassword(
+        Request $request,
+        UsersRepository $usersRepository
+        ): Response
+        {
+            $form = $this->createForm(ResetPasswordRequestFormType::class);
+            // on traite le formulaire , 
+            // je gere la requete avec le composant Request de httpfoundation
+            $form->handleRequest($request);
+
+            if($form->isSubmitted() && $form->isValid()){
+                //on vz chercher l'utilisateur par son email
+                $user = $usersRepository->findOneByEmail($form->get('email')->getData());
+                
+                //on verifie si on a un utilisateur
+                if($user){
+
+                }
+                // si $user n'existe pas
+                $this->addFlash('danger', 'Un problème est survenu');
+
+                return $this->redirectToRoute('app_login');
+
+
+            }
+
+            return $this->render('security/reset_password_request.html.twig', [
+                'requestPassForm' => $form->createView()
+            ]);
+        }
 }
