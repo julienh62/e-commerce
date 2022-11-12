b47fe2c0327c6d279522b3c6342668bb79dc54c1
LAPTOP-C6IOEE1R <jhennebo@gmail.com>
2022-11-12 19:27:54 +0100

    debut acces donnees

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/products', name: 'products_')]
+#[Route('/produits', name: 'products_')]
 class ProductsController extends AbstractController
 {
     #[Route('/', name: 'index')]
@@ -16,10 +16,10 @@ class ProductsController extends AbstractController
         return $this->render('products/index.html.twig');
     }
     #[Route('/{slug}', name: 'details')]
-    public function details(Products $products): Response
+    public function details(Products $product): Response
     {
 
         return $this->render('products/details.html.twig',
-            compact('products'));
+            compact('product'));
     }
 }
